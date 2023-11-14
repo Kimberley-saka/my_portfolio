895930392542788f361b2b06f8095f6c5525244e
Kimberley saka <kimberleysaka@gmail.com>
2023-11-14 09:10:17 +0200

    Updated routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import NavBar from "./components/Navigation/NavBar";
+import AboutPage from "./pages/About";
 import HomePage from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
+import ProjectsPage from "./pages/Projects";
+import GeospatialProducts from "./pages/Geospatial";
+import ContactPage from "./pages/Contact";
 
 export default function App() {
   return(
@@ -8,6 +12,10 @@ export default function App() {
       <NavBar/>
       <Routes>
         <Route path="/" element={<HomePage/>} ></Route>
+        <Route path="/about" element={<AboutPage/>} ></Route>
+        <Route path="/projects" element={<ProjectsPage/>} ></Route>
+        <Route path="/geospatial" element={<GeospatialProducts/>} ></Route>
+        <Route path="/contact" element={<ContactPage/>} ></Route>
       </Routes>
     </div>
   )
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ export default function AboutPage() {
     return(
         <div>
 
-            <p>Home</p>
+            <p>About</p>
         </div>
     )
 }

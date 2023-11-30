bdc97e6d39ab3d5122cebd4b8aeaf2ea4aeb4301
Kimberley-saka <kimberleysaka@gmail.com>
2023-11-30 09:38:44 +0200

    added hover styles

--- src/components/Navigation/NavBar.jsx
+++ src/components/Navigation/NavBar.jsx
@@ -3,15 +3,19 @@ export default function NavBar() {
   return(
     <div className="flex flex-row justify-between items-center h-20 md:px-28
      text-white text-lg">
-        <div className="flex text-3xl font-bold">
+        <div className="flex text-3xl font-bold hover:transform hover:scale-110 transition-transform">
           <Link to="/">KS</Link>
         </div>
 
         <div className="hidden md:flex space-x-12 items-center" >
-          <Link to="/projects">Projects</Link>
-          <Link to="/about">About</Link>
-          <Link to="/geospatial">Geospatial</Link>
-          <Link to="/contact">Contact</Link>
+          <Link to="/projects" className="
+          hover:transform hover:scale-110 transition-transform">Projects</Link>
+          <Link to="/about"
+          className="hover:transform hover:scale-110 transition-transform">About</Link>
+          <Link to="/geospatial"
+          className="hover:transform hover:scale-110 transition-transform">Geospatial</Link>
+          <Link to="/contact"
+          className="hover:transform hover:scale-110 transition-transform">Contact</Link>
         </div>
   </div>
 )

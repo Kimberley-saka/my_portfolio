5003f94fdbbdc90c1da02bab98402ae04c728106
Kimberley-saka <kimberleysaka@gmail.com>
2023-12-11 19:40:47 +0200

    added a few details

--- src/components/Navigation/NavBar.jsx
+++ src/components/Navigation/NavBar.jsx
@@ -3,8 +3,12 @@ export default function NavBar() {
   return(
     <div className="flex flex-row justify-between items-center h-20 md:px-28
      text-teal-600 text-lg">
-        <div className="flex text-3xl font-bold hover:transform hover:scale-110 transition-transform">
-          <Link to="/">KS</Link>
+        <div className="flex rounded-full text-3xl font-bold hover:transform 
+        hover:scale-110 transition-transform">
+          <Link to="/" className="flex mt-6 rounded-full bg-white">
+            <img src="/src/assets/images/kim.jpeg" alt="Kimberley" 
+            className="h-16 w-16 object-contain rounded-full"/>
+          </Link>
         </div>
 
         <div className="hidden md:flex space-x-12 items-center  text-lg" >
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 function About() {
   return (
-    <div>
-      About
+    <div className='flex flex-row text-lg'>
+      <div className='flex h-1/2 w-2/3 object-cover'>
+        <img src='/src/assets/images/kim.jpeg' className='object-cover bg-black'></img>
+      </div>
+
+      <div>
+        <p>Hi, I'm Kimberley Saka—a passionate Software Engineer from Malawi.
+          I thrive on turning code into solutions, specializing in Python,
+          SQL, and crafting RESTful APIs with Flask and Django.</p>
+
+      </div>
     </div>
   )
 }

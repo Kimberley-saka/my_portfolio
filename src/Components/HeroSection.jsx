bf6ed1c1a63a44c65b6e53a45fe616831a53b380
Kimberley-saka <kimberleysaka@gmail.com>
2026-02-06 18:17:44 +0200

    added motion

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -2,8 +2,9 @@ import GetInTouch from "./GetInTouch";
 
 function HeroSection() {
     return(
-        <div className="flex flex-col gap-14 text-white h-full justify-center items-center">
-        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-center">
+        <div className="flex flex-col gap-5 px-3 text-white h-1/2 justify-center items-center 
+        md:gap-14 md:px-5 md:h-full">
+        <h1 className="text-3xl text-left md:text-4xl lg:text-5xl font-bold md:text-center">
             Hello I'm Kimberley, a Geospatial Analyst who also loves to code backend software
         </h1>
         <div>

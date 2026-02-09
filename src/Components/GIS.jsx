32d0681883b23e84337fbcc8804f159c7f2500b1
Kimberley-saka <kimberleysaka@gmail.com>
2026-02-09 21:15:32 +0200

    updated

--- src/Components/GIS.jsx
+++ src/Components/GIS.jsx
@@ -1,9 +1,13 @@
 function GIS() {
     return (
-        <div className="h-150 ">
-            <h1>A few projects I'm proud of</h1>
+        <div className="flex flex-col text-dark-blue md:h-150 bg-light-orange ">
+            <h1 className="text-4xl md:text-6xl md:ml-20 pt-10">
+                A few GIS projects I'm proud of</h1>
             <div>
-                
+
+            </div>
+            <div>
+
             </div>
         </div>
     )

080146fa67a1007c547803fc18f6f908cbfa88a7
Kimberley-saka <kimberleysaka@gmail.com>
2026-02-24 15:11:52 +0200

    updated

--- src/Components/GIS.jsx
+++ src/Components/GIS.jsx
@@ -18,11 +18,11 @@ function GIS() {
     }
   ];
   return (
-    <div className="flex flex-col text-dark-blue  bg-light-orange md:px-10 md:h-150">
+    <div className="flex flex-col text-dark-blue  bg-light-orange px-10 md:h-150">
       <h1 className="text-4xl md:text-5xl md:pt-10">
       A few of my GIS projects</h1>
 
-      <div className="flex flex-col justify-center items-center h-full md:gap-20 md:flex-row">
+      <div className="flex flex-col h-full md:gap-20 md:flex-row">
        {projects.map((project, index) => 
       
       <Card 
--- src/Components/Software.jsx
+++ src/Components/Software.jsx
@@ -1,19 +1,40 @@
 //Software projects page
+import Card from "./Card";
+
 
 function Software() {
+
+  const projects = [
+    {
+      projectTitle: "project 1",
+      projectDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate."
+    },
+    {
+      projectTitle: "project 2",
+      projectDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate."
+    },
+    {
+      projectTitle: "project 3",
+      projectDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate."
+    }
+  ];
   return (
-    <div>
-      <div className="">
-        Software Projects
-          
-      </div>
+    <div className="flex flex-col text-dark-blue  bg-light-orange md:px-10 md:h-150">
+      <h1 className="text-4xl md:text-5xl md:pt-10">
+       Software projects</h1>
 
-      <div>
+      <div className="flex flex-col justify-center items-center h-full md:gap-20 md:flex-row">
+       {projects.map((project, index) => 
+      
+      <Card 
+      key={index} 
+      projectTitle={project.projectTitle}
+      projectDescription={project.projectDescription}/>
+      )}
 
+        </div>
       </div>
-    </div>
-  )
+    )
 }
-
 
 export default Software;
